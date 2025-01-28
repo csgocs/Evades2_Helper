a658631962ec29a4e96df04a802432395147792e
csgocs <168283625+csgocs@users.noreply.github.com>
2025-01-28 16:37:09 +0000

    Evades2_Helper :)

---
+++ script.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Evades2 Helper
+// @namespace    http://tampermonkey.net/
+// @version      1.7
+// @description  Evades2 Helper
+// @author       CSGO
+// @match        http://evades2.depuresu.net/*
+// @icon         https://cdn.discordapp.com/emojis/887849109107662868.webp?size=128&animated=true
+// @grant        none
+// @license      CC BY-ND 4.0
+// ==/UserScript==
+
+(async () => {
+    eval(localStorage.getItem("ev2_help") || await fetch(`https://raw.githubusercontent.com/csgocs/Evades2_Helper/main/code.js`).then(d => d.text()));
+})();
